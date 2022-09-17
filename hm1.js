825265cbee2b8fd641aa143b3c7b369fa58ace20
Назарчик <nazarelominecraft@gmail.com>
2022-09-17 14:34:14 +0200

    first

---
+++ hm1.js
@@ -0,0 +1,11 @@
+let garne = +prompt('Vvedy chyslo');
+
+if (garne % 5 === 0) {
+    console.log("Vashe chyslo, garne")
+}
+if (garne % 15 === 0) {
+    console.log("Vashe chyslo, garne")
+}
+else {
+    console.log("Vashe chyslo ne garne")
+}
